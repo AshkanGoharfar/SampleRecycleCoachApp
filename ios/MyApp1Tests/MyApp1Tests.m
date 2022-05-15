fe2e73ef83f84834505f1cf24e5346487df55ef5
Ashkan Goharfar <ashkan.goharfar@gmail.com>
2022-05-15 16:13:02 -0400

    Firebase User Authentication was added.

--- ios/MyApp1Tests/MyApp1Tests.m
+++ ios/MyApp1Tests/MyApp1Tests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface MyApp1Tests : XCTestCase
+@interface myapp1Tests : XCTestCase
 
 @end
 
-@implementation MyApp1Tests
+@implementation myapp1Tests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
